21611fa4d720e2a40734f045a757b5c2ec443b55
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-07-06 20:06:04 -0400

    Add eslint-plugin-vitest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,17 @@
 /* eslint-env node */
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
   parser: "@typescript-eslint/parser",
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:testing-library/react",
+    "plugin:vitest/recommended",
   ],
   plugins: ["@typescript-eslint"],
 };
