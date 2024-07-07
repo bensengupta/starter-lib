207f9c38c6c67664ab41c832d38e727601dce3f5
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-07-06 20:42:38 -0400

    Add cjs build

--- rollup.config.js
+++ rollup.config.js
@@ -9,11 +9,18 @@ const packageJson = require("./package.json");
 /** @type {import('rollup').RollupOptions} */
 export default {
   input: "src/main.ts",
-  output: {
-    file: "dist/main.mjs",
-    format: "esm",
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: "dist/main.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: "dist/main.mjs",
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
   plugins: [typescript({ tsconfig: "./tsconfig.build.json" })],
   external: [
     ...Object.keys(packageJson.dependencies),
